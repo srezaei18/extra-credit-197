7207b421d445359b9e0d0f8c55f34850549b88ef
srezaei18 <43484788+srezaei18@users.noreply.github.com>
2018-09-21 18:58:39 -0400

    Add files via upload

---
+++ ec.c
@@ -0,0 +1,24 @@
+//Sason Rezaei
+//Extra Credit 1
+//Enter integer
+//Fri, Sept 21
+//
+
+
+
+#include <stdio.h>
+
+int main(void) {
+
+	int STDOUT;
+
+	printf("Please enter an integer\n");
+	scanf("%d", &STDOUT);
+
+	printf("%d\n", STDOUT);
+
+
+
+
+	return 0;
+}
